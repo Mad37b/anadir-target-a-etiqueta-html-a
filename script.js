bb3942e6ad1fab70559a5457f6077791a5795f4b
Mad37b <Miguelkit37@hotmail.com>
2024-07-05 14:38:40 +0200

    segundo commit

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 // buscar en la <a > --> <a href enlaces target=_blank
 // funcion añadirTargetfiltrado
 var etiquetaEnlaces = document.querySelectorAll("a")
-var rutas = document.querySelectorAll ( "href")
-var regex = RegExp('/^(https?:\/\/)/')
-var archivos = ['.jpg', 'png', 'pdf',];
-var texto = ['https://'+''+'com','www.google.com','http://eurostarshotelsunificado.com/','https://www.eurostarshotels.com/'];
-if (etiquetaEnlaces != null) {
+var regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/;
+var archivos = ['.jpg', '.png', '.pdf',];
+if (etiquetaEnlaces.length) {
     // URL 
-    if (regex != null) {
-        for (var i = 0; i < etiquetaEnlaces.length; i++) {
-            var enlace = etiquetaEnlaces[i];
-            console.log(enlace)
-
-
-                 // ya tengo la etiqueta a imprimida en la consola , lo que quiero hacer es saber si <a href> contiene https  y archivo,
-                //obtendrá target 
-            // si contiene formato archivo , añadir target
+    for (var i = 0; i < etiquetaEnlaces.length; i++) {
+        var enlace = etiquetaEnlaces[i].href;
+        if(enlace.match(regex)[1] == location.hostname){
             for(var j = 0 ; j < archivos.length; j++){
-                if (archivos.includes(archivos[j])) {
-                    // añadir target 
-                    var ventanaArchivos = enlace.setAttribute("target", "_blank");
+                if (enlace.endsWith(archivos[j])) {
+                    var ventanaArchivos = etiquetaEnlaces[i].setAttribute("target", "_blank");
                 }
-
             }
-            
+        }else{
+            console.log(enlace + ' --> no coincide')
+        }
+        console.log(enlace )
+       
+        
 
-            // si contiene enlace externo ,añadir target por cada enlace externo
-
-            if (rutas){
-
-                var ventanasUrl = enlace.setAttribute("target","_blank");
-            }
-            
-        }// fin del for 
     }
 }
