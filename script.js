3333871db3c56c1c1c98b58a3f74a080c5874df1
Mad37b <Miguelkit37@hotmail.com>
2024-07-08 13:50:50 +0200

    Segudo commit , comprueba si existe el tag a y el href , añade con exito el atributo target solo en los formatos de imagenes .png .jpg y aparte .pdf en cualquier parte de la linea

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-// buscar en la <a > --> <a href enlaces target=_blank
+// buscar en la <a> --> <a href enlaces target=_blank
 // funcion añadirTargetfiltrado
 var etiquetaEnlaces = document.querySelectorAll("a")
 var regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/;
 var archivos = ['.jpg', '.png', '.pdf',];
-if (etiquetaEnlaces.length) {
-    // URL 
+var dominios = [".com", ".es", "net", "org"];
+var esImagen; false;
+
+//1. comprueba si la etiqueta existe por lo menos alguno 
+if (etiquetaEnlaces.length > 0) {
+    // 1. verifica si existe href 
+
     for (var i = 0; i < etiquetaEnlaces.length; i++) {
         var enlace = etiquetaEnlaces[i].href;
-        if(enlace.match(regex)[1] == location.hostname){
-            for(var j = 0 ; j < archivos.length; j++){
-                if (enlace.endsWith(archivos[j])) {
-                    var ventanaArchivos = etiquetaEnlaces[i].setAttribute("target", "_blank");
-                }
+        console.log("test 1 " + etiquetaEnlaces[i].href);
+
+        if (enlace.match(regex)[1] == location.hostname) {
+            console.log("Aqui esta la URL de la pagina web ----->" + window.location.hostname);
+        }// cierra 1º if 
+
+
+        // 2. Añadir target i son imagenes 
+        for (var j = 0; j < archivos.length; j++) {
+            if (enlace.endsWith(archivos[j])) {
+                esImagen = true;
+                console.log("test 2");
             }
-        }else{
-            console.log(enlace + ' --> no coincide')
+
+            // mira si hay png en cualquier parte de la url 
+            if (!esImagen && enlace.includes(".png") || enlace.includes(".jpg") || enlace.includes(".pdf")) {
+                esImagen = true;
+                console.log ( ' mira si hay imagenes ')
+            }
+
         }
-        console.log(enlace )
-       
-        
+
+
+        if (esImagen) {
+
+            etiquetaEnlaces[i].setAttribute('target', '_blank');
+            console.log("Enlace a imagen detectado:", enlace);
+        } else {
+            console.log("Enlace no es una imagen:", enlace);
+        }
 
     }
 }
+
