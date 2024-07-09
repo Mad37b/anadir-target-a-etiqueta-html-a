c8f1d6baa2d48a991cdfedb1968352454183e1c8
Mad37b <Miguelkit37@hotmail.com>
2024-07-09 18:00:01 +0200

    quito commit , se ha mejorado el codigo y se ha aplicado mejor lógica , aplicando nuevos metodos como some y nuevas funciones

--- script.js
+++ script.js
@@ -1,61 +1,32 @@
 
-(function () {
-
-    //variables 
-    var etiquetaEnlaces = document.querySelectorAll("a")
+(function (etiquetaEnlaces) {
+    if (!etiquetaEnlaces.length) return false;
     var regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/;
     var archivos = ['.jpg', '.png', '.pdf',];
-    var host = location.href;
-    console.log ( host);
-    var contador = 0;
-    //funciones 
+    var host = location.hostname;
     function anadirTarget(enlace) {
-        if (enlace) {
-            enlace.setAttribute('target', '_blank');
-            console.log("Target añadido al Enlace:" + enlace.href);
-        }
+        enlace.setAttribute('target', '_blank');
     }
     function removerTarget(enlace) {
-        if(enlace){
         enlace.removeAttribute('target');
-        console.log ( "target removido del Enlace "+ enlace.href)
     }
-    }
-    // 1.  
-    if (etiquetaEnlaces.length > 0) {
-        // 1. verifica si existe href 
-        // Tag A //
-        for (var i = 0; i < etiquetaEnlaces.length; i++) {
-            // atributo href //
-        var enlace = etiquetaEnlaces[i].href;
-        var href = enlace.href;
-        console.log ( " prueba en el for " + enlace ) ; 
-       
-                // 2. 
-            if (etiquetaEnlaces[i] != host) {
-                anadirTarget(etiquetaEnlaces[i]);
-               
-                console.log(" Se añade target al enlace Externo  " + etiquetaEnlaces[i])
 
-            } else {
+    for (var i = 0; i < etiquetaEnlaces.length; i++) {
+        var enlace = etiquetaEnlaces[i],
+        href = enlace.href,
+        dominio = href.match(regex);
+        if (dominio[1] == host) {
+            var esArchivo = archivos.some(function (archivos) {
+                return href.endsWith(archivos);
+            });
+            if (esArchivo) {
+                anadirTarget(enlace);
+            }else{
                 removerTarget(etiquetaEnlaces[i]);
-                console.log(" No se añade target al enlace Interno " + host)
             }
+        } else {
+            anadirTarget(enlace);
         }
-       
-        // 3. Añadir target si son imagenes 
-        for (var j = 0; j < archivos.length; j++) {
-            if (enlace.endsWith(archivos[j]) !=host) {
-                anadirTarget(etiquetaEnlaces[i]);
-            }
-            // mira si hay png en cualquier parte de la url 
-            if (enlace.includes(".png") || enlace.includes(".jpg") || enlace.includes(".pdf")) {
-                anadirTarget(etiquetaEnlaces[i]);
-            }
-
-        }
-
-
     }
 
-})();
+}) (document.querySelectorAll("a"));
